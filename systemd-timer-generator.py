5dbeed9c0530f44a859d14506c5da252c08b0fe5
Darren Kameoka <48581401+dkameoka@users.noreply.github.com>
2023-01-21 16:12:21 -1000

    style: adhere to PEP8 through Flake8

--- systemd-timer-generator.py
+++ systemd-timer-generator.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
-#pylint: disable=broad-except,logging-fstring-interpolation,missing-docstring,too-many-arguments
+# pylint: disable=invalid-name,missing-docstring,too-many-arguments
 
 import argparse
 import csv
 import subprocess
 from pathlib import Path
 
+
 class SystemdTimerExc(Exception):
     """ SystemdTimer Exception """
 
+
 class SystemdTimer:
 
-    def __init__(self,outdir,name,cals,exec_,force):
+    def __init__(self, outdir, name, cals, exec_, force):
         self.outdir = Path(outdir)
         self.name = self._validate_name(name)
         self.cals = self._validate_cals(cals)
@@ -35,11 +37,11 @@ class SystemdTimer:
     def _validate_name(name):
         name = name.strip()
         name_len = len(name)
-        name_max = 256 - len('.service') # implicitly shorter than with ".timer" suffix.
+        name_max = 256 - len('.service')  # implicitly shorter than with ".timer" suffix.
         if name_len == 0 or name_len > name_max:
             raise SystemdTimerExc(f'Name "{name_len}" is empty or longer than {name_max}')
         for char in name:
-            if not char.isalnum() and char not in [':','-','_','.','\\']:
+            if not char.isalnum() and char not in [':', '-', '_', '.', '\\']:
                 raise SystemdTimerExc(f'Name "{name}" has an invalid character "{char}"')
         return name
 
@@ -47,18 +49,18 @@ class SystemdTimer:
     def _validate_cal(cal):
         cal = cal.strip()
         try:
-            subprocess.run(['systemd-analyze','calendar',cal,'--iterations','0'],check = True)
+            subprocess.run(['systemd-analyze', 'calendar', cal, '--iterations', '0'], check=True)
         except subprocess.CalledProcessError as exc:
             raise SystemdTimerExc(f'Calendar datetime "{cal}" is invalid') from exc
         except FileNotFoundError:
             print('systemd-analyze is missing. Skipping calendar validation.')
         return cal
 
-    def _validate_cals(self,cals):
+    def _validate_cals(self, cals):
         cals = cals.split(';')
         if len(cals) == 0:
             raise SystemdTimerExc('Empty calendar')
-        return list(map(self._validate_cal,cals))
+        return list(map(self._validate_cal, cals))
 
     def timer(self):
         return (
@@ -82,11 +84,11 @@ class SystemdTimer:
     def service(self):
         return (
             '[Unit]\n'
-           f'Description=Automatically generated\n'
+            f'Description=Automatically generated\n'
             'After=network.target\n\n'
             '[Service]\n'
             'Type=oneshot\n'
-           f'ExecStart={self.exec}\n')
+            f'ExecStart={self.exec}\n')
 
     def service_write(self):
         outpath = self.outdir / f'{self.name}.service'
@@ -97,27 +99,29 @@ class SystemdTimer:
             file_.write(self.service())
         print(f'Wrote {outpath}')
 
+
 class _Main:
 
-    def __init__(self,conf,outdir,force):
+    def __init__(self, conf, outdir, force):
         self.conf = conf
         self.outdir = outdir
         self.force = force
 
-    def process_row(self,row):
-        timer = SystemdTimer(self.outdir,row['name'],row['cals'],row['exec'],self.force)
+    def process_row(self, row):
+        timer = SystemdTimer(self.outdir, row['name'], row['cals'], row['exec'], self.force)
         timer.timer_write()
         timer.service_write()
 
     def load_conf(self):
-        with self.conf.open('r',newline = '') as file:
-            fieldnames = ('name','cals','exec')
-            for row in csv.DictReader(file,fieldnames = fieldnames,delimiter = '|',restval = ''):
+        with self.conf.open('r', newline='') as file:
+            fieldnames = ('name', 'cals', 'exec')
+            for row in csv.DictReader(file, fieldnames=fieldnames, delimiter='|', restval=''):
                 name = row['name']
                 try:
                     self.process_row(row)
                 except SystemdTimerExc as exc:
                     print(f'"{name}" error: {exc}')
+
 
 def _file_type(value):
     path = Path(value)
@@ -125,31 +129,33 @@ def _file_type(value):
         raise ValueError(f'{value} is not a file path')
     return path
 
+
 def _dir_type(value):
     path = Path(value)
     if not path.is_dir():
         raise ValueError(f'{value} is not a directory path')
     return path
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = 'Generates persistent Systemd timer services')
+    parser = argparse.ArgumentParser(description='Generates persistent Systemd timer services')
     parser.add_argument(
-        dest = 'conf',
-        type = _file_type,
-        help = 'PSV config with: name | cals[;] | exec')
+        dest='conf',
+        type=_file_type,
+        help='PSV config with: name | cals[;] | exec')
     parser.add_argument(
-        '-o','--outdir',
-        dest = 'outdir',
-        type = _dir_type,
-        default = Path('/etc/systemd/system/'),
-        help = 'Output path. Default: /etc/systemd/system/')
+        '-o', '--outdir',
+        dest='outdir',
+        type=_dir_type,
+        default=Path('/etc/systemd/system/'),
+        help='Output path. Default: /etc/systemd/system/')
     parser.add_argument(
-        '-f','--force',
-        dest = 'force',
-        action = 'store_true',
-        help = 'Overwrite existing timer and service files')
+        '-f', '--force',
+        dest='force',
+        action='store_true',
+        help='Overwrite existing timer and service files')
 
     args = parser.parse_args()
 
-    main = _Main(args.conf,args.outdir,args.force)
+    main = _Main(args.conf, args.outdir, args.force)
     main.load_conf()
